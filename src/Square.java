c24c48930f0559057f7636c02cd63b0c0d3dd12a
guyzam1r <55858722+guyzam1r@users.noreply.github.com>
2020-02-25 14:37:21 -0800

    solving for easy/medium sudokus

--- src/Square.java
+++ src/Square.java
@@ -1,60 +1,64 @@
 import java.util.ArrayList;
 
-public class Box {
-	int value, index, rowNum, colNum, boxNum;
+public class Square {
+	
+	int value;
+	int index;
 	ArrayList<Integer> posNums = new ArrayList<Integer>();
-	int boxSize = 9;
-
-	public Box(int value, int index) {
+	int rowNum;
+	int colNum;
+	int bSquareNum;
+	
+	public Square(int value, int index){
 		this.value = value;
 		this.index = index;
-		rowNum = index / boxSize;
-		colNum = index % boxSize;
-		// boxNum = bsn;
+		rowNum = index / 9;
+		colNum = index % 9;
+		//bSquareNum = bsn;
 	}
-
-	public Box(int index) {
+	
+	public Square(int index) {
 		this.index = index;
-		rowNum = index / boxSize;
-		colNum = index % boxSize;
-		// boxNum = bsn;
-		for (int i = 1; i <= boxSize; i++) {
+		rowNum = index / 9;
+		colNum = index % 9;
+		//bSquareNum = bsn;
+		for (int i = 1; i <= 9; i++) {
 			posNums.add(i);
 		}
 	}
-
+	
 	public void setValue(int val) {
 		value = val;
 	}
-
+	
 	public int getValue() {
 		return value;
 	}
-
+	
 	public int getIndex() {
 		return index;
 	}
-
+	
 	public int getRow() {
 		return rowNum;
 	}
-
+	
 	public int getCol() {
 		return colNum;
 	}
-
-	public int getBoxNum() {
-		return boxNum;
+	
+	public int getBigSquare() {
+		return bSquareNum;
 	}
-
+	
 	public ArrayList<Integer> getPosNums() {
 		return posNums;
 	}
-
+	
 	public boolean isFilled() {
 		return posNums.size() == 0;
 	}
-
+	
 	public void fill() {
 		value = posNums.get(0);
 		posNums.clear();
--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,53 +1,73 @@
+import java.util.Arrays;
+
 public class Sudoku {
-	Box[] Boxs;
-	int totalCells = 81;
 
-	public static void main(String[] args) {
-
-	}
-
-	public Sudoku(int[] arr) {
-		Boxs = new Box[totalCells];
-		for (int i = 0; i < totalCells; i++) {
-			if (arr[i] == 0) {
-				Boxs[i] = new Box(i);
-			} else {
-				Boxs[i] = new Box(arr[i], i);
-			}
-		}
-	}
-
-	public int[] Solve() {
-		int[] arr = new int[totalCells];
-		int emptyBoxs = totalCells;
-		// initialize posNums
-		Box cur;
-		for (int i = 0; i < totalCells; i++) {
-			cur = Boxs[i];
-			if (cur.isFilled()) {
-				emptyBoxs--;
-			}
-		}
-		// while remaining Boxs:
-		while (emptyBoxs > 0) {
-			for (int i = 0; i < totalCells; i++) {
-				cur = Boxs[i];
-				if (cur.getPosNums().size() == 1) {
-					cur.fill();
-					updateBoxs(cur.getIndex());
-				}
-			}
-		}
-		// confirm Box with posNums of length 1
-		// then, update posNums of Boxs with shared row, column, bigBox
-
+	Square[] squares;
+    
+    public Sudoku(int[] arr) {
+    	squares = new Square[81];
+    	for (int i = 0; i < 81; i++) {
+    		if (arr[i] == 0) {
+    			squares[i] = new Square(i);
+    		} else {
+    			squares[i] = new Square(arr[i], i);
+    		}
+    	}
+    }
+    
+    public int[] Solve() {
+    	int[] arr = new int[81];
+    	int emptySquares = 81;
+    	//initialize posNums
+    	Square cur;
+    	for (int i = 0; i < 81; i++) {
+    		cur = squares[i];
+    		if (cur.isFilled()) {
+    			arr[i] = cur.getValue();
+    			updateSquares(i);
+    			emptySquares--;
+    		}
+    	}
+    	//while remaining squares:
+    	int pes = emptySquares;
+    	while (emptySquares > 0) {
+    		
+    		for (int i = 0; i < 81; i++) {
+    			cur = squares[i];
+    			if (!cur.isFilled() && cur.getPosNums().size() == 1) {
+    				cur.fill();
+    				arr[i] = cur.getValue();
+    				updateSquares(i);
+    				emptySquares--;
+    			}
+    		}
+    		
+    	}
 		return arr;
-
-	}
-
-	public void updateBoxs(int index) {
-		// for all unfilled Boxs in row, col, and bigBox --> remove # of given
-		// index from posnums
-
-	}
+    	
+    }
+    
+    public void updateSquares(int index) {
+    	//for all unfilled squares in row, col, and bigsquare --> remove # of given index from posnums
+    	//row
+    	for (int i = index/9*9; i < index/9*9+9; i++) {
+    		if (!squares[i].isFilled()) {
+    			squares[i].getPosNums().remove(Integer.valueOf(squares[index].getValue()));
+    		}
+    	}
+    	//col
+    	for (int i = index % 9; i < 81; i += 9) {
+    		if (!squares[i].isFilled()) {
+    			squares[i].getPosNums().remove(Integer.valueOf(squares[index].getValue()));
+    		}
+    	}
+    	//bsquare
+    	int start = index - (((index / 9) % 3) * 9) - (index % 3);
+    	int[] nums = {start, start + 1, start + 2, start + 9, start + 10, start + 11, start + 18, start + 19, start + 20};
+    	for (int i : nums) {
+    		if (!squares[i].isFilled()) {
+    			squares[i].getPosNums().remove(Integer.valueOf(squares[index].getValue()));
+    		}
+    	}
+    }
 }
