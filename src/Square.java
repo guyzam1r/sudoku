6bcbe71d384eb91f7ab084e92b6b7ae9b91c39d0
guyzam1r <55858722+guyzam1r@users.noreply.github.com>
2020-02-28 14:37:15 -0800

    Update Square.java

--- src/Square.java
+++ src/Square.java
@@ -1,60 +1,68 @@
 import java.util.ArrayList;
 
 public class Square {
-	int value, index, rowNum, colNum, bSquareNum;
-	int boxSize = 9;
+	
+	int value;
+	int index;
 	ArrayList<Integer> posNums = new ArrayList<Integer>();
-
-	public Square(int value, int index) {
+	int rowNum;
+	int colNum;
+	int bSquareNum;
+	
+	public Square(int value, int index){
 		this.value = value;
 		this.index = index;
-		rowNum = index / boxSize;
-		colNum = index % boxSize;
-		// bSquareNum = bsn;
+		rowNum = index / 9;
+		colNum = index % 9;
+		//bSquareNum = bsn;
 	}
-
+	
 	public Square(int index) {
 		this.index = index;
-		rowNum = index / boxSize;
-		colNum = index % boxSize;
-		// bSquareNum = bsn;
-		for (int i = 1; i <= boxSize; i++) {
+		rowNum = index / 9;
+		colNum = index % 9;
+		//bSquareNum = bsn;
+		for (int i = 1; i <= 9; i++) {
 			posNums.add(i);
 		}
 	}
-
+	
 	public void setValue(int val) {
 		value = val;
 	}
-
+	
 	public int getValue() {
 		return value;
 	}
-
+	
 	public int getIndex() {
 		return index;
 	}
-
+	
 	public int getRow() {
 		return rowNum;
 	}
-
+	
 	public int getCol() {
 		return colNum;
 	}
-
+	
 	public int getBigSquare() {
 		return bSquareNum;
 	}
-
+	
 	public ArrayList<Integer> getPosNums() {
 		return posNums;
 	}
-
+	
+	public void rmFromPosNums(int n) {
+		posNums.remove(Integer.valueOf(n));
+	}
+	
 	public boolean isFilled() {
 		return posNums.size() == 0;
 	}
-
+	
 	public void fill() {
 		value = posNums.get(0);
 		posNums.clear();
